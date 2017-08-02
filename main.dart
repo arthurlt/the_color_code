5cb33f48d30ab2ca25c6361ca3cf39b6e5bb9e56
Arthur Tucker <arthurlt@gmail.com>
2017-08-02 16:52:30 -0600

    We can now get each question

--- main.dart
+++ main.dart
@@ -2,7 +2,29 @@ import 'dart:io';
 import 'dart:convert';
 
 main() async {
+    int questionNumber = 0;
     String jsonFile = await new File('questions.json').readAsString();
+
+
     List questionList = JSON.decode(jsonFile);
-    print(questionList[0]);
+    String singleQuestionJson = JSON.encode(questionList[questionNumber]);
+    //print(singleQuestionJson);
+
+    Map singleQuestionMap = JSON.decode(singleQuestionJson);
+    //print(singleQuestionMap["choices"]);
+
+    String singleQuestionQuestionString = singleQuestionMap["question"];
+    print(singleQuestionQuestionString);
+    String choicesJson = JSON.encode(singleQuestionMap["choices"]);
+    //print(choicesJson);
+
+    for (int i = 0; i < singleQuestionMap["choices"].length; i++) {
+        List singleChoiceList = JSON.decode(choicesJson);
+        //print(singleChoiceList[i]);
+
+        String singleChoiceJson = JSON.encode(singleChoiceList[i]);
+        Map singleChoiceMap = JSON.decode(singleChoiceJson);
+
+        print("${singleChoiceMap["answer"]}?");
+    }
 }
