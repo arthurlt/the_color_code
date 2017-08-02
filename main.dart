abb35a3e964d47d3f9c1d9baf366e39899cae463
Arthur Tucker <arthurlt@gmail.com>
2017-08-02 17:14:52 -0600

    figured out how to do correctly work with nested objects

--- main.dart
+++ main.dart
@@ -2,29 +2,14 @@ import 'dart:io';
 import 'dart:convert';
 
 main() async {
-    int questionNumber = 0;
+    int questionNumber = 14;
     String jsonFile = await new File('questions.json').readAsString();
 
-
     List questionList = JSON.decode(jsonFile);
-    String singleQuestionJson = JSON.encode(questionList[questionNumber]);
-    //print(singleQuestionJson);
 
-    Map singleQuestionMap = JSON.decode(singleQuestionJson);
-    //print(singleQuestionMap["choices"]);
+    print(questionList[questionNumber]["question"]);
 
-    String singleQuestionQuestionString = singleQuestionMap["question"];
-    print(singleQuestionQuestionString);
-    String choicesJson = JSON.encode(singleQuestionMap["choices"]);
-    //print(choicesJson);
-
-    for (int i = 0; i < singleQuestionMap["choices"].length; i++) {
-        List singleChoiceList = JSON.decode(choicesJson);
-        //print(singleChoiceList[i]);
-
-        String singleChoiceJson = JSON.encode(singleChoiceList[i]);
-        Map singleChoiceMap = JSON.decode(singleChoiceJson);
-
-        print("${singleChoiceMap["answer"]}?");
+    for (int i = 0; i < questionList[questionNumber]["choices"].length; i++) {
+        print("${questionList[questionNumber]["choices"][i]["answer"]}?");
     }
 }
