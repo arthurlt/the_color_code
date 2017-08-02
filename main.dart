a04609c7e1ff2f06db7905c1c3457ecdfcda8b55
Arthur Tucker <arthurlt@gmail.com>
2017-08-02 14:48:08 -0600

    Further intial configuration

--- main.dart
+++ main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'dart:convert';
 
 main() async {
-    var jsonFile = await new File('questions.json').readAsString();
+    String jsonFile = await new File('questions.json').readAsString();
     List questionList = JSON.decode(jsonFile);
     print(questionList[0]);
 }
