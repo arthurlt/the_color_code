d6dcb2965050f678d420e3cc8ae8c03ecfab6ed7
Arthur Tucker <arthurlt@gmail.com>
2017-08-02 18:07:41 -0600

    fixed random space in json, basic question classes created

--- main.dart
+++ main.dart
@@ -5,16 +5,22 @@ main() async {
     String jsonFile = await new File('questions.json').readAsString();
 
     List questionList = JSON.decode(jsonFile);
-
     
     for (int i = 0; i < questionList.length; i++) {
         print(questionList[i]["question"]);
-        
         for (int j = 0; j < questionList[i]["choices"].length; j++) {
             print("${questionList[i]["choices"][j]["answer"]}?");
         }
 
         print(" ");
-    }
-    
+    }   
+}
+
+class Question {
+    int questionNumber;
+    String questionString, redAnswer, blueAnswer, whiteAnswer, yellowAnswer;
+}
+
+class PhraseQuestion extends Question {
+    String redDef, blueDef, whiteDef, yellowDef;
 }
